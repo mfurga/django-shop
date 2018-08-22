abebe1f92daa8dcd0195b75ec07c150e626ed1ac
mfurga <mfurga@protonmail.com>
2018-08-22 14:41:10 +0200

    Added media & static url to shop urls.py. Updated ads views & common.css

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Ad
+
 
 def ads_list(request):
-    return render(request, 'ads/ads_list.html', {})
+    ads = Ad.objects.all()
+    return render(request, 'ads/ads_list.html', {'ads':ads})
