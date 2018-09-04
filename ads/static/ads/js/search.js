a1d68e852496ff2d41192f2463b719079ffb4b3b
mfurga <mfurga@protonmail.com>
2018-09-04 09:31:47 +0200

    Added sorting by time & price

--- ads/static/ads/js/search.js
+++ ads/static/ads/js/search.js
@@ -3,6 +3,46 @@ $( document ).ready(function() {
     var url = new URLSearchParams(window.location.search);
     var id = null;
 
+    if (url.has('sort_time')) setFilter('#time', url.get('sort_time'));
+    if (url.has('sort_price')) setFilter('#price', url.get('sort_price'));
+
+    function setFilterUrl(element) {
+        var value = 'up';
+
+        if (url.has(element)) {
+            value = url.get(element);
+            if (value === 'up') value = 'down';
+            else if (value === 'down') value = 'up';
+        }
+
+        url.set(element, value);
+        var parameterToRemove = 'sort_time';
+        if (element === 'sort_time') parameterToRemove = 'sort_price';
+        url.delete(parameterToRemove);
+        window.location = '?' + url.toString()
+    }
+
+    function setFilter(element, value) {
+        var icon = $(element + ' i');
+        value = value || 'up';
+
+        if (value === 'up'){
+            icon.removeClass('fa-arrow-down').addClass('fa-arrow-up');
+            $(element).addClass('fbold');
+        } else if (value === 'down') {
+            icon.removeClass('fa-arrow-up').addClass('fa-arrow-down');
+            $(element).addClass('fbold');
+        }
+    }
+
+    $('#time').click(function () {
+        setFilterUrl('sort_time');
+    });
+
+    $('#price').click(function () {
+        setFilterUrl('sort_price');
+    });
+
     if (url.has('category')) {
         id = url.get('category');
         var category = $('#cat-' + id);
@@ -26,6 +66,5 @@ $( document ).ready(function() {
         $(this).append(input);
 
         $(this).find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
-
     });
 })
