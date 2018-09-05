d480a1e6c6db7423d7bfc29827572b5f3de3b572
mfurga <mfurga@protonmail.com>
2018-09-05 13:00:55 +0200

    Added ads forms.py

--- ads/forms.py
+++ ads/forms.py
@@ -51,3 +51,75 @@ class AdSearchFrom(forms.Form):
             'class': 'checkbox'
         })
     )
+
+
+class AdForm(forms.Form):
+    title = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(attrs={
+            'class': 'text_input',
+            'placeholder': _('Title')
+        })
+    )
+    category = forms.ChoiceField(
+        choices=CATEGORIES,
+        widget=forms.Select(attrs={
+            'class': 'choices-list'
+        })
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'text_area'
+        })
+    )
+    condition = forms.ChoiceField(
+        choices=CONDITIONS,
+        widget=forms.Select(attrs={
+            'class': 'choices-list'
+        })
+    )
+    photo = forms.ImageField(
+        required=False,
+        widget=forms.FileInput(attrs={
+            'id': 'photo',
+            'class': 'photo'
+        })
+    )
+    photo2 = forms.ImageField(
+        required=False,
+        widget=forms.FileInput(attrs={
+            'id': 'photo2',
+            'class': 'photo'
+        })
+    )
+    photo3 = forms.ImageField(
+        required=False,
+        widget=forms.FileInput(attrs={
+            'id': 'photo3',
+            'class': 'photo'
+        })
+    )
+    photo4 = forms.ImageField(
+        required=False,
+        widget=forms.FileInput(attrs={
+            'id': 'photo4',
+            'class': 'photo'
+        })
+    )
+    location = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(attrs={
+            'class': 'text_input',
+            'placeholder': _('Location')
+        })
+    )
+    price = forms.IntegerField(
+        widget=forms.NumberInput(attrs={
+            'class': 'text_input'
+        })
+    )
+    phone = forms.IntegerField(
+        widget=forms.NumberInput(attrs={
+            'class': 'text_input tel'
+        })
+    )
