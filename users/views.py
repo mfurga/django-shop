a6908720ed966af20e046f1e99c52bcd7cd5abe3
mfurga <mfurga@protonmail.com>
2018-09-11 09:29:22 +0200

    Created users signin view. Updated signin template & locale messages

--- users/views.py
+++ users/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils.translation import gettext_lazy as _
+from django.shortcuts import render, reverse
+from django.contrib import messages
+
+from .forms import SigninForm
 
 
 def users_signin(request):
-    return render(request, 'users/users_signin.html', {})
+    next_page = request.GET.get('next') or reverse('ads:list')
+    invalid_credentials = None
+    form = SigninForm()
+
+    if request.method == 'POST':
+        form = SigninForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            invalid_credentials = True
+
+            if user is not None:
+                invalid_credentials = False
+                login(request, user)
+                messages.success(request, _('Logged in successfully.'))
+                return HttpResponseRedirect(next_page)
+
+    context = {
+        'form': form,
+        'invalid_credentials': invalid_credentials
+    }
+    return render(request, 'users/users_signin.html', context)
